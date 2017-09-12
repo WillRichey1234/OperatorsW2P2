a888a8acdb622a79fa5bedf51bcc94e927f0cf32
WillRichey1234 <gsampson@vms.edu>
2017-09-12 11:56:47 -0600

    Hopefully this works

---
+++ OperatorsW2P2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace OperatorsW2P2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // declaring all of the integers that will be used
+            // decleration statement
+            int x, y, a, b;
+
+            //Assigment operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            //Math Operators
+            // Addition
+            x = 3 + 4;
+
+            //Subtraction
+            x = 4 - 3;
+
+            // multiplacation
+            x = 10 * 5;
+
+            // division
+            x = 10 / 5;
+
+            // equality 
+            if (x == y)
+            [
+                ]
+                //Greater then
+            if (x > y)
+            {
+                
+            }
+            //Less than
+            if (x < y)
+            {
+                
+            }
+            //Greater than or equal to
+            if( x >= y)
+            {
+                
+            }
+            //Less than or equal to
+            if (x <= y)
+            {
+                
+            }
+            //Conditional operators
+            // and 
+            if ((x > y) && (a == b))
+            {
+                
+            }
+            // or
+            if ((x == y) || (a > b))
+            {
+                
+            }
+            // in line conditional operator
+            string message = (x == 1) ? "car" : "Boat";
+            //Message acces and Method Incovation
+            Console.WriteLine("hehe");
+        }
+    }
+}
